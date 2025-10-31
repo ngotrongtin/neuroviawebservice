a318d31d0abc5027bd7941a87afa6eb04f15f4c4
ngotrongtin <tigopro.1703@gmail.com>
2025-10-31 11:22:21 +0700

    fix the csv bug

--- app/controllers/mqtt_messages_controller.rb
+++ app/controllers/mqtt_messages_controller.rb
@@ -1,14 +1,16 @@
 class MqttMessagesController < ApplicationController
   def index
-    @messages = MqttMessage.order(received_at: :desc).page(params[:page]).per(20)
-
     respond_to do |format|
-    format.html
-    format.csv do
-      headers['Content-Disposition'] = "attachment; filename=\"mqtt_messages_#{Time.now.strftime('%Y%m%d_%H%M%S')}.csv\""
-      headers['Content-Type'] ||= 'text/csv'
+      format.html do
+        @messages = MqttMessage.order(received_at: :desc).page(params[:page]).per(20)
+      end
+
+      format.csv do
+        @messages = MqttMessage.order(received_at: :desc)
+        headers['Content-Disposition'] = "attachment; filename=\"mqtt_messages_#{Time.now.strftime('%Y%m%d_%H%M%S')}.csv\""
+        headers['Content-Type'] ||= 'text/csv'
+      end
     end
-  end
   end
 
   def show
